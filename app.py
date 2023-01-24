801714cc994752b5f322482d1f1bd0a2bcb67acf
Peter Newton <peter@4sightdesigns.com>
2023-01-24 12:39:30 +0000

    Chore: comment out unused functions

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ class App:
     _connection: Connection
     _server: Server
     _control: Control
-    _screen: str
+    # _screen: str
 
     def __init__(self) -> None:
         self._events = Events()
@@ -31,16 +31,16 @@ class App:
         self._server = Server(self._events, self._control, self._logger)
         self._control = Control(self._events)
         self._server = Server(self._events, self._control, self._logger)
-        self._screen = ""
+        # self._screen = ""
 
-    def set_screen(self, screen: str) -> None:
-        print("set screen: ", screen)
-        self._screen = screen
-        self._events.publish(ACTIONS.DISPLAY_SET_SCREEN, self._screen)
+    # def set_screen(self, screen: str) -> None:
+    #     print("set screen: ", screen)
+    #     self._screen = screen
+    #     self._events.publish(ACTIONS.DISPLAY_SET_SCREEN, self._screen)
 
     def setup(self) -> None:
         self._connection.connect()
-        self.set_screen("home")
+        # self.set_screen("home")
 
     async def start(self) -> None:
         asyncio.create_task(self._server.start())
@@ -50,31 +50,31 @@ class App:
         # asyncio.create_task(self._control.debug_channels())
 
     async def run(self) -> None:
-        self._events.subscribe(ACTIONS.BUTTON_PRESSED, self.handle_inputs)
+        # self._events.subscribe(ACTIONS.BUTTON_PRESSED, self.handle_inputs)
         while True:
             await asyncio.sleep(0.1)
 
-    def handle_inputs(self, buttons) -> None:
-        if self._screen == "home":
-            if buttons['x'].is_pressed:
-                self._events.publish(ACTIONS.LOG_LEVEL_INC, None)
-            if buttons['y'].is_pressed:
-                self.set_screen('home')
-            if buttons['a'].is_pressed:
-                self.set_screen('settings')
-            if buttons['b'].is_pressed:
-                self.set_screen('control')
-        elif self._screen == "control":
-            if buttons['y'].is_pressed:
-                self.set_screen('home')
-            if buttons['a'].is_pressed:
-                self.set_screen('home')
-            if buttons['b'].is_pressed:
-                self.set_screen('settings')
-        elif self._screen == "settings":
-            if buttons['y'].is_pressed:
-                self.set_screen('home')
-            if buttons['a'].is_pressed:
-                self.set_screen('control')
-            if buttons['b'].is_pressed:
-                self.set_screen('home')
+    # def handle_inputs(self, buttons) -> None:
+    #     if self._screen == "home":
+    #         if buttons['x'].is_pressed:
+    #             self._events.publish(ACTIONS.LOG_LEVEL_INC, None)
+    #         if buttons['y'].is_pressed:
+    #             self.set_screen('home')
+    #         if buttons['a'].is_pressed:
+    #             self.set_screen('settings')
+    #         if buttons['b'].is_pressed:
+    #             self.set_screen('control')
+    #     elif self._screen == "control":
+    #         if buttons['y'].is_pressed:
+    #             self.set_screen('home')
+    #         if buttons['a'].is_pressed:
+    #             self.set_screen('home')
+    #         if buttons['b'].is_pressed:
+    #             self.set_screen('settings')
+    #     elif self._screen == "settings":
+    #         if buttons['y'].is_pressed:
+    #             self.set_screen('home')
+    #         if buttons['a'].is_pressed:
+    #             self.set_screen('control')
+    #         if buttons['b'].is_pressed:
+    #             self.set_screen('home')
